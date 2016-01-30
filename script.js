51b266acbee54085f67ccbf520793c2719e3f46b
Nujerk <junkerlucas@gmail.com>
2016-01-30 13:47:59 +0100

    Fix tilemap

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ GameTemplate.Game = function (game) {
 
 GameTemplate.Game.prototype = {
     preload: function() {
-        this.load.pack("main", "assets/main_pack.json");
+        //this.load.pack("main", "assets/main_pack.json");
+        this.load.tilemap('map', 'assets/newtest/newtest.json', null, Phaser.Tilemap.TILED_JSON);
+
+        this.load.image('enemy', 'assets/newtest/enemy.png');
+        this.load.image('test 1', 'assets/newtest/tileset_test1.png');
     },
 
     create: function() {
@@ -20,8 +24,9 @@ GameTemplate.Game.prototype = {
 
         this.physics.arcade.gravity.y = 1500;
 
-        var map = this.add.tilemap("stage1");
-        map.addTilesetImage("test1", "tileset_test1");
+        var map = this.add.tilemap("map");
+        map.addTilesetImage("enemy");
+        map.addTilesetImage("test 1");
 
         map.setCollisionBetween(0, 6569);
 
@@ -33,10 +38,7 @@ GameTemplate.Game.prototype = {
         var enemies = this.add.group();
         enemies.enableBody = true;
 
-        map.createFromObjects('Livables', 6, 'enemy', 0, true, false, enemies);
-        console.log(enemies);
-
-        // sprite = this.add.sprite(0, 0, "player");
+        map.createFromObjects('Livable', 5, 'enemy', 0, true, false, enemies);
     },
 
     update: function() {

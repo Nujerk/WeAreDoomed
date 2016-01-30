6f8c3eb85ae6a54b396d45bb961a3b2ed5cdef88
Scarounet <rondeau.clement@gmail.com>
2016-01-30 09:43:33 +0100

    Init

---
+++ script.js
@@ -0,0 +1,17 @@
+var GameTemplate = {};
+
+GameTemplate.Game = function (game) {
+
+};
+
+GameTemplate.Game.prototype = {
+    preload: function() {
+    },
+
+    create: function() {
+        this.stage.backgroundColor = 0x2c3e50;
+    },
+
+    update: function() {
+    },
+}

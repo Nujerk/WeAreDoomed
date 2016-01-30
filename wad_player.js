0166ef82f4fdfc30b358140b8b5baabf3a5da5ce
Nujerk <junkerlucas@gmail.com>
2016-01-30 15:42:07 +0100

    Commit fixed control for player

--- wad_player.js
+++ wad_player.js
@@ -10,6 +10,10 @@ WADPlayer = function (game, x, y) {
     this.leftAnimation = null;
     this.rightAnimation = null;
     this.inputsInit(game);
+    game.input.gamepad.start();
+    this.pad = null;
+    if(game.input.gamepad.pad1.connected)
+		this.pad = game.input.gamepad.pad1;
 };
 
 WADPlayer.prototype = Object.create(Phaser.Sprite.prototype);
@@ -19,38 +23,41 @@ WADPlayer.prototype.constructor = WADPlayer;
  * Automatically called by World.update
  */
 WADPlayer.prototype.update = function() {
-	if(this.leftKey.isDown || this.buttonDPadLeft.isDown)
+	if(this.leftKey.isDown)
 		this.moveLeft();
-	else if(this.rightKey.isDown || this.buttonDPadRight.isDown)
+	else if(this.rightKey.isDown)
 		this.moveRight();
-	else if(this.leftKey.isUp || this.rightKey.isUp || this.buttonDPadLeft.isUp || this.buttonDPadRight.isUp)
+	else if(this.leftKey.isUp || this.rightKey.isUp)
 		this.stop();
 };
 
 WADPlayer.prototype.inputsInit = function(game){
-	/// Keyboard inputs
-	this.upKey 		= game.input.keyboard.addKey(Phaser.Keyboard.Z);
-	this.downKey 	= game.input.keyboard.addKey(Phaser.Keyboard.S);
-	this.leftKey 	= game.input.keyboard.addKey(Phaser.Keyboard.Q);
-	this.rightKey 	= game.input.keyboard.addKey(Phaser.Keyboard.D);
-	this.fireKey 	= game.input.keyboard.addKey(Phaser.Keyboard.CONTROL);
-	this.specialKey = game.input.keyboard.addKey(Phaser.Keyboard.SHIFT);
-	this.jumpKey 	= game.input.keyboard.addKey(Phaser.Keyboard.A);
-	this.lockKey 	= game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-	this.rollKey 	= game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
-
-	/// Pad inputs
-	game.input.gamepad.start();
-    pad = game.input.gamepad.pad1;
-
-    this.buttonA 			= pad.getButton(Phaser.Gamepad.XBOX360_A);
-    this.buttonB 			= pad.getButton(Phaser.Gamepad.XBOX360_B);
-    this.buttonX 			= pad.getButton(Phaser.Gamepad.XBOX360_X);
-    this.buttonY 			= pad.getButton(Phaser.Gamepad.XBOX360_Y);
-    this.buttonDPadLeft 	= pad.getButton(Phaser.Gamepad.XBOX360_DPAD_LEFT);
-    this.buttonDPadRight 	= pad.getButton(Phaser.Gamepad.XBOX360_DPAD_RIGHT);
-    this.buttonDPadUp 		= pad.getButton(Phaser.Gamepad.XBOX360_DPAD_UP);
-    this.buttonDPadDown 	= pad.getButton(Phaser.Gamepad.XBOX360_DPAD_DOWN);
+	if(this.pad == null)
+	{
+		/// Keyboard inputs
+		this.upKey 		= game.input.keyboard.addKey(Phaser.Keyboard.Z);
+		this.downKey 	= game.input.keyboard.addKey(Phaser.Keyboard.S);
+		this.leftKey 	= game.input.keyboard.addKey(Phaser.Keyboard.Q);
+		this.rightKey 	= game.input.keyboard.addKey(Phaser.Keyboard.D);
+		this.fireKey 	= game.input.keyboard.addKey(Phaser.Keyboard.CONTROL);
+		this.specialKey = game.input.keyboard.addKey(Phaser.Keyboard.SHIFT);
+		this.jumpKey 	= game.input.keyboard.addKey(Phaser.Keyboard.A);
+		this.lockKey 	= game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		this.rollKey 	= game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+	}
+	else
+	{
+		/// Pad inputs
+	    this.upKey 		= pad.getButton(Phaser.Gamepad.XBOX360_DPAD_UP);
+	    this.downKey 	= pad.getButton(Phaser.Gamepad.XBOX360_DPAD_DOWN);
+	    this.leftKey 	= pad.getButton(Phaser.Gamepad.XBOX360_DPAD_LEFT);
+	    this.rightKey 	= pad.getButton(Phaser.Gamepad.XBOX360_DPAD_RIGHT);
+	    this.fireKey 	= pad.getButton(Phaser.Gamepad.XBOX360_X);
+	    this.specialKey	= pad.getButton(Phaser.Gamepad.XBOX360_B);
+	    this.jumpKey 	= pad.getButton(Phaser.Gamepad.XBOX360_A);
+	    this.lockKey 	= pad.getButton(Phaser.Gamepad.XBOX360_XBOX360_LEFT_TRIGGER);
+	    this.rollKey 	= pad.getButton(Phaser.Gamepad.XBOX360_XBOX360_RIGHT_TRIGGER);
+	}
 };
 
 WADPlayer.prototype.moveLeft = function(){
